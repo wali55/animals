aed68e91c81ce39520ff542687347caabb65588c
wali55 <walisantunu@gmail.com>
2023-04-09 22:12:14 +0600

    image added

---
+++ src/AnimalShow.js
@@ -0,0 +1,24 @@
+import bird from './svg/bird.svg';
+import cat from './svg/cat.svg';
+import cow from './svg/cow.svg';
+import dog from './svg/dog.svg';
+import gator from './svg/gator.svg';
+import heart from './svg/heart.svg';
+import horse from './svg/horse.svg';
+
+const svgMap = {
+    bird,
+    cat,
+    cow,
+    dog,
+    gator,
+    horse
+}
+
+function AnimalShow({ type }) {
+    return <div>
+        <img src={svgMap[type]} alt="animal" />
+    </div>
+}
+
+export default AnimalShow;
--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import AnimalShow  from './AnimalShow';
+
+function getRandomAnimal() {
+  const animals = ['bird', 'cat', 'cow', 'gater', 'horse'];
+
+  return animals[Math.floor(Math.random() * animals.length)];
+}
 
 function App() {
+    const [animals, setAnimals] = useState([]);
+
+    const handleClick = () => {
+      setAnimals([...animals, getRandomAnimal()]);
+    };
+
+    const renderedAnimals = animals.map((animal, index) => {
+      return <AnimalShow type={animal} key={index} />
+    });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={handleClick}>Add Animal</button>
+      <div>{renderedAnimals}</div>
     </div>
   );
 }
